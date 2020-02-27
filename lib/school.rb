157098bed4bf85cd5eee9f3457aa6d6cc3004875
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-27 03:58:41 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+class School
+
+
+def initialize(name)
+  @name = name
+  @roster = {}
+ end
+ 
+ def roster
+   @roster
+ end
+ 
+ def add_student(student_names, grade)
+  if 
+    @roster.has_key?(grade)
+    @roster[grade] << student_names
+  else
+    @roster[grade] = []
+    @roster[grade] << student_names
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+   @roster.each do |key, value|
+     value.sort!
+   end
+  end
+ end
+end
